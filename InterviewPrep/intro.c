c88421986b30f47a83f51bf1e6ba87cd14ce78b4
Sivakumar S <125854895+Sivask-23@users.noreply.github.com>
2024-06-22 21:41:13 +0530

    Add files via upload

---
+++ InterviewPrep/intro.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main(){
+
+
+
+int x=5;
+int y=2;
+
+float res=x/(float)y;
+
+printf("helloo%8.2f\n",res);
+
+printf("%d\n",x!=y); //return true as 1 and false as 0
+
+if (x=0)
+{
+    printf("hello");
+
+}else{
+    printf("else part");
+}
+
+
+
+
+
+    return 0;
+}
